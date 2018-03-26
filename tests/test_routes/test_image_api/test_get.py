0ad50e493d453c10eb13a639468a39ce4331d56e
linw1995 <linw1995@icloud.com>
2018-03-27 00:12:13 +0800

    impove unit test

--- tests/test_routes/test_image_api/test_get.py
+++ tests/test_routes/test_image_api/test_get.py
@@ -1,7 +1,7 @@
 from cloud_img.models import Image, ImageWithUploadCfg, UploadCfg
 
 
-async def test_get_image(db_manager, faker, logined_client, user):
+async def test_get_image_pagination(db_manager, faker, logined_client, user):
     resp = await logined_client.get('/image')
     assert resp.status == 200
     data = await resp.json()
@@ -10,6 +10,36 @@ async def test_get_image(db_manager, faker, logined_client, user):
     assert data['page_no'] == 1
     assert data['page_size'] == 15
 
+    for i in range(100):
+        await db_manager.create(Image, user=user)
+
+    resp = await logined_client.get('/image')
+    assert resp.status == 200
+    data = await resp.json()
+    assert data['total'] == 100
+    assert data['images'][0]['id'] == 100
+    assert data['page_no'] == 1
+    assert data['page_size'] == 15
+
+    resp = await logined_client.get('/image?page_no=2')
+    assert resp.status == 200
+    data = await resp.json()
+    assert data['total'] == 100
+    assert data['images'][0]['id'] == 85
+    assert data['page_no'] == 2
+    assert data['page_size'] == 15
+
+    resp = await logined_client.get('/image?page_no=2&page_size=10')
+    assert resp.status == 200
+    data = await resp.json()
+    assert data['total'] == 100
+    assert data['images'][0]['id'] == 90
+    assert data['page_no'] == 2
+    assert data['page_size'] == 10
+
+
+async def test_get_image_with_multi_sources(db_manager, faker, logined_client,
+                                            user):
     upload_cfg_a = await db_manager.create(UploadCfg, user=user, name='test_a')
     upload_cfg_b = await db_manager.create(UploadCfg, user=user, name='test_b')
     for i in range(100):
@@ -21,18 +51,18 @@ async def test_get_image(db_manager, faker, logined_client, user):
             await db_manager.create(
                 ImageWithUploadCfg, upload_cfg=upload_cfg_b, image=image)
 
-    resp = await logined_client.get('/image')
+    resp = await logined_client.get('/image?page_no=10&page_size=10')
     assert resp.status == 200
     data = await resp.json()
-    assert data['total'] == 100
-    assert data['images'][0]['id'] == 100
-    assert data['page_no'] == 1
-    assert data['page_size'] == 15
+    images = data['images']
+    first_img = images[-1]
+    second_img = images[-2]
+    thrid_img = images[-3]
+    assert first_img['id'] == 1
+    assert len(first_img['sources']) == 0
 
-    resp = await logined_client.get('/image?page_no=2&page_size=10')
-    assert resp.status == 200
-    data = await resp.json()
-    assert data['total'] == 100
-    assert data['images'][0]['id'] == 90
-    assert data['page_no'] == 2
-    assert data['page_size'] == 10
+    assert second_img['id'] == 2
+    assert len(second_img['sources']) == 2
+
+    assert thrid_img['id'] == 3
+    assert len(thrid_img['sources']) == 1
